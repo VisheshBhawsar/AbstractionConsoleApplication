cb971e337adca733876020ea493050460a991149
Vishesh <Vishesh.bhawsar@emids.com>
2016-09-30 12:01:45 +0530

    Add project files.

---
+++ AbstractionConsoleApplication/Factory.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.Configuration;
+
+namespace AbstractionConsoleApplication
+{
+    public class Factory
+    {
+        #region Container Methods
+
+        private static IUnityContainer _container;
+        private static readonly object ContainerAccess = new object();
+        private static bool _isConfigured;
+
+        public static IUnityContainer Container
+        {
+            get
+            {
+                Init();
+                return _container;
+            }
+            set { _container = value; }
+        }
+
+        /// <summary>
+        /// Initialized the unity container and reads its configuration
+        /// </summary>
+        public static void Init()
+        {
+            if (_container == null || _isConfigured == false)
+            {
+                lock (ContainerAccess)
+                {
+                    if (_container == null || _isConfigured == false)
+                    {
+                        try
+                        {
+                            _container = new UnityContainer();
+                            _container.LoadConfiguration();
+                            _isConfigured = true;
+                        }
+                        catch (Exception ex)
+                        {
+                            // Debug.WriteLine("Unity Load Error!!! - " + ex.Message);
+                            throw new Exception(
+                                "Unity Configuration Error - Check the <unity> section of your config file.", ex);
+                        }
+                    }
+                }
+            }
+        }
+
+        #endregion
+
+        //public static T CreateInstance<T>()
+        //{
+        //    // this is where Unity takes over and does its magic.
+        //    var obj = Container.Resolve<T>();
+        //    return obj;
+        //}
+
+        public static T CreateInstance<T>()
+        {
+            var obj = Container.Resolve<T>();
+            return obj;
+        }
+    }
+}
---
+++ AbstractionConsoleApplication/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractionConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person pp = new Person();
+            pp.Run();
+        }
+    }
+
+
+    public class BaseClass<T> where T : class
+    {
+        public IBase<T> classObject;
+
+        public BaseClass()
+        {
+            classObject = Factory.CreateInstance<T>() as IBase<T>;
+        }
+    }
+
+    public class Person : BaseClass<IRead>
+    {
+        public void Run()
+        {
+            if (classObject != null)
+            {
+                classObject.GetInstance().GetAll();
+            }
+        }
+    }
+
+
+    public interface IRead : IBase<ReadClass>
+    {
+        string GetAll();
+    }
+
+    public class ReadClass : IRead
+    {
+        public string GetAll()
+        {
+            return "GetRead";
+        }
+
+        public ReadClass GetInstance()
+        {
+            return new ReadClass();
+        }
+    }
+
+    public interface IWrite : IBase<WriteClass>
+    {
+        string GetWrite();
+    }
+
+    public class WriteClass : IWrite
+    {
+        public string GetWrite()
+        {
+            return "GetWrite";
+        }
+
+        public WriteClass GetInstance()
+        {
+            return new WriteClass();
+        }
+    }
+
+    public interface IBase<T>
+    {
+        T GetInstance();
+    }
+}
+
